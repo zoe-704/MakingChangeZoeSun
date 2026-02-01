923af1175613c238416380a4f9888f4fc2521ad1
zoe-704 <sunzoey@gmail.com>
2026-01-31 20:15:38 -0800

    Final making change

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -9,8 +9,39 @@ import java.util.Arrays;
  */
 
 public class MakingChange {
+    /*
+    static long[][] dp; // table to store amounts from memoization
+    static int[] c;
+
+    public static long dfs(int i, int amount) {
+        // base cases: exact amount made OR negative amount / no coins left
+        if (amount == 0) return 1;
+        if (amount < 0 || i == c.length) return 0;
+        if (dp[i][amount] != -1) return dp[i][amount]; // return if already computed
+
+        long useCoin = dfs(i, amount-c[i]); // dfs and use coin at index i
+        long skipCoin = dfs(i+1, amount); // dfs and skip coin
+
+        // memoize results
+        dp[i][amount] = useCoin + skipCoin;
+        return dp[i][amount];
+    }
+    */
+
     public static long countWays(int target, int[] coins) {
         int n = coins.length;
+        /* // memoization
+        dp = new long[n][target + 1];
+        c = coins;
+        // fill dp array with -1 to mark as uncomputed
+        for (int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
+        // dfs from index 0 to get target amount
+        return dfs(0, target);
+        */
+
+
+
+        /* // 2D tabulation
         long[][] dp = new long[n][target+1]; // table to store calculations from tabulation
         Arrays.sort(coins); // sorting coins makes code faster
 
@@ -25,21 +56,21 @@ public class MakingChange {
             }
         }
         return dp[n-1][target]; // number of ways to make target with all coins
-
-        /*
-        int n = coins.length;
+        */
 
 
+
+        // 1D tabulation
         long[] dp = new long[target + 1];
-        dp[0] = 1; // 1 way to make 0 cents
-
+        dp[0] = 1; // base case: 1 way to make 0 cents
+        // iterate through each coin value and 1-target amount of money
         for (int coin : coins) {
             for (int i = 1; i <= target; i++) {
-                if (i - coin >= 0 && i - coin < target) dp[i] += dp[i - coin];
+                // prevent going out of bounds
+                if (i - coin >= 0 && i - coin < target)
+                    dp[i] += dp[i - coin]; // calculate ways to make i amount of money
             }
         }
-
         return dp[target];
-         */
     }
 }
